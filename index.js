b5c3f63b409b90df6ad3d319a87ca7cb99586f78
adixfilip <adrianxfilip@gmail.com>
2023-11-02 14:51:45 +0200

    Changed cors origin

--- index.js
+++ index.js
@@ -662,13 +662,13 @@ var chat = []
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header({"Access-Control-Allow-Origin": "*"});
+  res.header({"Access-Control-Allow-Origin": "https://adrianxfilip.github.io/coinflip-client"});
   next();
 }) 
 
 const socketIO = require("socket.io")(http, {
   cors: {
-    origin: "*"
+    origin: "https://adrianxfilip.github.io/coinflip-client"
   },
 });
 
