b64407c3a96bc4c3ac28d88a7e98f0cb1a81c81a
adixfilip <adrianxfilip@gmail.com>
2023-11-14 19:51:02 +0200

    Overall functionality updates finished

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ var roomTemplate = {
     socketID: "",
     playerID: "",
     side: "",
+    name : ""
   },
   playerTwo: {
     socketID: "",
     playerID: "",
     side: "",
+    name : ""
   },
   bet: 0,
   status: "closed",
@@ -98,23 +100,26 @@ socketIO.on("connection", (socket) => {
   socket.broadcast.emit("connected-users-update", socketIO.engine.clientsCount);
 
   socket.on("disconnect", () => {
+    var hadOpenRooms = false
     Object.keys(rooms).forEach((room) => {
       if (
         rooms[room].playerOne.socketID == socket.id &&
         rooms[room].status != "ongoing"
       ) {
+        hadOpenRooms = true
         rooms[room] = {
           ...roomTemplate
         };
       }
     });
-    socket.broadcast.emit("rooms-update", rooms);
+    if(hadOpenRooms){
+      socket.broadcast.emit("rooms-update", rooms);
+    }
     socket.broadcast.emit("connected-users-update", socketIO.engine.clientsCount);
   });
 
   socket.on("create-room", (betData, userData) => {
     // for future update - current system allows for more than 5 rooms per player if there are closed rooms before the users open rooms 
-    console.log(betData,userData)
     var roomCount = 0;
     Object.keys(rooms).every((room) => {
       if (rooms[room].playerOne.socketID == socket.id) {
@@ -145,22 +150,23 @@ socketIO.on("connection", (socket) => {
     }
   });
 
-  socket.on("join-room", (roomData) => {
-    roomID = roomData.roomID;
-    userData = roomData.userData;
+  socket.on("join-room", (roomID, userData) => {
     const sides = ["heads", "tails"];
     const winningSide = sides[Math.floor(Math.random() * sides.length)];
     rooms[roomID] = {
       ...rooms[roomID],
       playerTwo: {
-        id: socket.id,
+        socketID: socket.id,
         side: rooms[roomID].playerOne.side == "heads" ? "tails" : "heads",
         name: userData.name,
+        playerID : userData.playerID
       },
       status: "ongoing",
       winningSide: winningSide,
     };
-    socket.emit("balanceUpdate", rooms[roomID].bet * -1);
+    socketIO.emit("rooms-update", rooms);
+    socket.emit("balance-update", rooms[roomID].bet * -1);
+
     var balancePlayerOne =
       winningSide != rooms[roomID].playerOne.side
         ? 0
@@ -169,28 +175,18 @@ socketIO.on("connection", (socket) => {
       winningSide != rooms[roomID].playerTwo.side
         ? 0
         : rooms[roomID].bet * 2 - (10 / 100) * rooms[roomID].bet;
-    socketIO.emit("rooms", rooms);
+
     setTimeout(() => {
       socketIO
-        .to(rooms[roomID].playerTwo.id)
-        .emit("balanceUpdate", balancePlayerTwo);
+        .to(rooms[roomID].playerTwo.socketID)
+        .emit("balance-update", balancePlayerTwo);
       socketIO
-        .to(rooms[roomID].playerOne.id)
-        .emit("balanceUpdate", balancePlayerOne);
+        .to(rooms[roomID].playerOne.socketID)
+        .emit("balance-update", balancePlayerOne);
       rooms[roomID] = {
-        playerOne: {
-          id: "",
-          side: "",
-        },
-        playerTwo: {
-          id: "",
-          side: "",
-        },
-        bet: 0,
-        status: "closed",
-        winningSide: "",
+        ...roomTemplate
       };
-      socketIO.emit("rooms", rooms);
+      socketIO.emit("rooms-update", rooms);
     }, 5000);
   });
 
